677114fda4fd9170eb5a7a194925324d1f01f4f0
Jacek C. Wojdel <j.c.wojdel@gmail.com>
2010-09-09 12:44:13 +0000

    - added sorting of the phonons in phonons.py - added ASR in phonons.py - changed phonons.py to a more common unix style way of giving options from commandline
    git-svn-id: svn+ssh://pcd313b/FDToolbox/trunk@126 8f901a96-2ad2-476c-acde-4028cff3aa0d

--- phonons.py
+++ phonons.py
@@ -4,24 +4,57 @@ from fdtoolbox.calculation_set import *
 from fdtoolbox.utility import *
 from fdtoolbox.linear_expansion import *
 import sys
+import getopt
+
+    
+try:                                
+  opts, args = getopt.getopt(sys.argv[1:], "zapmc:s", ["zmass","asr","polarity","mpolarity","condition=","sort"])
+except getopt.GetoptError:
+  print "Usage:"
+  print sys.argv[0] + "[options] directory_name easy_axis"
+  print "Options:"
+  print "-z, --zmass"
+  print "\tUse ZMASS from the output in order to calculate real phonons, not just force constant"
+  print "\tmatrix eigenvalues."
+  print "-a, --asr"
+  print "\tForce acoustic sum rule on the FC matrix."
+  print "-p, --polarity"
+  print "\tPrint out the mode polarities"
+  print "-m, --mpolarity"
+  print "\tPrint out magnetic strengths of the modes"
+  print "-c expr, --condition=expr"
+  print "\tPrint out only eigenmodes satisfying given condition."
+  print "\tThe condition is a Python expression, which can contain the following variables:"
+  print "\t\teigenvalue - mode eigenvalue"
+  print "\t\ti - unsorted eigenmode number"
+  print "-s, --sort"
+  print "\tSort the modes according to their eigenvalue. It also forces printing unsorted mode numbers."
+  sys.exit(2)                     
+
 
 try:
-  directory = sys.argv[1]
+  directory = args[0]
 except:
   directory = "."
 
+options = []
+condition = 'True'
+for opt, arg in opts:
+  if opt in ('-z','--zmass'):
+    options.append('Z')
+  if opt in ('-a','--asr'):
+    options.append('A')
+  if opt in ('-p','--polarity'):
+    options.append('P')
+  if opt in ('-m','--mpolarity'):
+    options.append('M')
+  if opt in ('-s','--sort'):
+    options.append('S')
+  if opt in ('-c','--condition'):
+    condition = arg
+    
 try:
-  condition = sys.argv[2]
-except:
-  condition = "True"
-
-try:
-  polarities = sys.argv[3]
-except:
-  polarities = ""
-  
-try:
-  saxis = [float(v) for v in sys.argv[4].split('/')]
+  saxis = [float(v) for v in args[1].split('/')]
 except:
   saxis = [0., 0., 1.]
     
@@ -44,27 +77,44 @@ lin_exp = linear_expansion( cs )
 lin_exp.calculate_expansion_coefficients()
 
 fc_matrix, units = lin_exp.force_constant_matrix()
-if 'Z' in polarities:
+
+if 'A' in options:
+  print 'Enforcing ASR'
+  for i in range(0,fc_matrix.shape[0],3):
+    asr = zeros([3,3])
+    for j in range(0,fc_matrix.shape[0],3):      
+      asr += fc_matrix[i:i+3,j:j+3]
+      
+    fc_matrix[i:i+3,i:i+3] -= asr
+    
+
+if 'Z' in options:
   print 'Using atomic masses in the calculations'
   units += '*amu**-1'
   for i in range(len(cs.groundstate.pomass)):
     fc_matrix[i*3:i*3+3,:] /= sqrt(cs.groundstate.pomass[i])
     fc_matrix[:,i*3:i*3+3] /= sqrt(cs.groundstate.pomass[i])
+    
 
 evalues, evectors = linalg.eig(fc_matrix)
 
 
-new_evalues=[]
-new_evectors=[]
+new_evv = []
 for i,eigenvalue in enumerate(evalues):
   if eval(condition):
-    new_evalues.append(eigenvalue)
-    new_evectors.append(evectors[:,i])
+    new_evv.append((eigenvalue,evectors[:,i],i))
     
-evalues = array(new_evalues)
-evectors = hstack(new_evectors)
+if 'S' in options:
+  new_evv = sorted(new_evv)
+evalues = array([e[0] for e in new_evv])
+evectors = hstack([e[1] for e in new_evv])
+enumbers = array([e[2] for e in new_evv])
 
-if 'Z' in polarities:
+if 'S' in options:
+  print 'Unsorted mode numbers:'
+  print mat2str(enumbers,'%10d')
+
+if 'Z' in options:
   print 'Phonon frequencies (cm**-1)'
   evalues[:]=[sign(a)*sqrt(abs(a)) for a in evalues]
   print mat2str(SQRTEVANGAMU_TO_CM*evalues.T, '%10.4f')
@@ -75,16 +125,11 @@ else:
 print 'Force constant matrix eigenmodes'
 print mat2str( evectors, '%10.4f' )
   
-if 'P' in polarities:   
+if 'P' in options:   
   print 'Dielectric polarity of the modes'
   print mat2str( dot( lin_exp.born_charges()[0].T, evectors ), '%10.4f' )
 
-if 'M' in polarities:
+if 'M' in options:
   print 'Magnetic "polarity" of the modes (1e-3)'
   print mat2str( 1e3*dot( lin_exp.B_m_mu.T, evectors ), '%10.4f' )
  
-
-#print 'Force constant matrix eigenvalues (%s)'%units
-#print mat2str( evalues.T, '%10.4f' )
-#print 'Force constant matrix eigenmodes'
-#print mat2str( evectors, '%10.4f' )
